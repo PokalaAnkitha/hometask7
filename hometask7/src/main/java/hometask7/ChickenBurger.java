580b3c41f5f6863f2f65c761e60cec9406da6bb4
PokalaAnkitha <50981933+PokalaAnkitha@users.noreply.github.com>
2020-03-09 18:03:18 +0530

    Add files via upload

---
+++ hometask7/src/main/java/hometask7/ChickenBurger.java
@@ -0,0 +1,11 @@
+package hometask7;
+public class ChickenBurger extends Burger {
+	
+	   public float price() {
+	      return 50.5f;
+	   }
+	  
+	   public String name() {
+	      return "Chicken Burger";
+	   }
+	}
---
+++ hometask7/src/main/java/hometask7/Container.java
@@ -0,0 +1,4 @@
+package hometask7;
+public interface Container {
+	   public Iterator getIterator();
+}
